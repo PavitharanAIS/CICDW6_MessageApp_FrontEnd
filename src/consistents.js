5353902c17e037571d49233485ced9bb375def6b
20232094 <para005@ess.ais.ac.nz>
2024-06-24 17:28:45 +1200

    Tested test and ci

--- src/consistents.js
+++ src/consistents.js
@@ -1,2 +1,2 @@
-export const BaseUrl = "https://cicd-week3-message.vercel.app//";
+export const BaseUrl = "https://cicd-week3-message.vercel.app/";
 // export const BaseUrl = "http://localhost:3001/"
